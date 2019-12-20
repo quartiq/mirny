1722b35cf193d82cf89c8a628a764716bfff1107
Etienne Wodey <wodey@iqo.uni-hannover.de>
2019-12-20 15:00:15 +0100

    flash_xc3: use standard parameter expansion to default CABLE

--- flash_xc3.sh
+++ flash_xc3.sh
@@ -3,7 +3,7 @@
 set -e
 
 XC3SPROG=xc3sprog
-CABLE=$([[ -z "$1" ]] && echo "xpc" || echo "$1")
+CABLE=${1-xpc}
 
 set -x
 $XC3SPROG -c $CABLE -m /opt/Xilinx/14.7/ISE_DS/ISE/xbr/data -v build/mirny.jed:w
